1430fce414686ecab6ad571b0ce5eddc0f41dd40
ESther Ng'ang'a <esther.nganga@student.moringaschool.com>
2022-10-20 16:53:18 +0300

    commit tree f2492c14e0c4422a84ecf1818651440f9b4f52ae

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require_relative '../lib/dog'
 # files.
 #
 # Given that it is always loaded, you are encouraged to keep this file as
-# light-weight as possible. Requiring heavyweight dependencies from this file
+# light-weigzht as possible. Requiring heavyweight dependencies from this file
 # will add to the boot time of your test suite on EVERY test run, even for an
 # individual file that may not need all of that loaded. Instead, consider making
 # a separate helper file that requires the additional dependencies and performs
